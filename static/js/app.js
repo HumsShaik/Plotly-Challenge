2f241113d96505bf46cdb374d9ff367862108b98
Hums Shaik <humsshaik@gmail.com>
2021-04-23 13:22:10 -0500

    moved samples.json to data

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,31 @@
 console.log("app.js loaded");
 
-
+// the code in these files is based on Dom's office hours
 
 function InitDashboard(){
     console.log("InitDashboard()");
+
+    // Populate the dropdown
+    var selector = d3.select("#selDataset");
+
+
+    // read data from samples.json and use this data to populate the dropdown
+    d3.json("data/samples.json").then(function(data) {
+		console.log(data);
+
+    });
+
+    // Update the bargraph
+    // Updadate the bubblechart
+    // Update the demographic informaation
+
+
+   
+
+
+
+
+
 }
 
 InitDashboard();
