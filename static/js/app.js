b4c54aea5c519d3fde7f0519b256079b48c96f0b
Hums Shaik <humsshaik@gmail.com>
2021-04-23 13:35:42 -0500

    functions created

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,23 @@ console.log("app.js loaded");
 
 // the code in these files is based on Dom's office hours
 
+function DrawBargraph(sampleId) {
+	console.log(`DrawBargraph(${sampleId})`);
+
+}
+
+function DrawBubblechart(sampleId) {
+	console.log(`DrawBubblechart(${sampleId})`);
+	
+}
+
+
+function ShowMetadata(sampleId) {
+	console.log(`ShowMetadata(${sampleId})`);
+	
+}
+
+
 function InitDashboard(){
     console.log("InitDashboard()");
 
@@ -15,14 +32,21 @@ function InitDashboard(){
 
 
         var sampleNames = data.names;
-
+    // dropdown populated
         sampleNames.forEach(sampleId => {
             selector.append("option")
                .text(sampleId)
                .property("value", sampleId);
         });
 
+     // generate code for dropdown
 
+        var id = sampleNames[0];
+
+        DrawBargraph(id);
+        DrawBubblechart(id);
+        ShowMetadata(id); 
+        //drawGuage(id);
     });
 
     // Update the bargraph
